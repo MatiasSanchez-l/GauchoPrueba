12975a0046a58148e53ebf80f6dd7d9daf64187a
LancerFate <matias-leandro@outlook.com>
2019-07-22 12:55:33 -0300

    se agrego camara perzonalizada

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,6 +7,9 @@ var gravedad = 700
 
 var movimiento = Vector2()
 
+#variables para la camara
+onready var cam = $Camera2D
+onready var manejo_camara = $Manejo_camara
 
 func _ready():
 	pass # Replace with function body.
@@ -25,3 +28,11 @@ func _physics_process(delta):
 		movimiento.y = salto
 		
 	movimiento = move_and_slide(movimiento, arriba)
+
+	#for para la camara
+	for area in manejo_camara.get_overlapping_areas():
+		if(area.is_in_group("Camara_ajuste")):
+			cam.limit_left = area.position.x
+			cam.limit_right = area.position.x + 640 * area.scale.x
+			cam.limit_top = area.position.y
+			cam.limit_bottom = area.position.y + 360 * area.scale.y
